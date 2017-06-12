7946c6e2002c866807913738bee7ddbbb7878208
govarshini <govarshiniarumugam@gmail.com>
2017-06-12 16:02:46 +0530

    Create number.java

---
+++ number.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class number
+{
+	public static void main(String args[])
+	{
+		Scanner s=new Scanner(System.in);
+		int x=s.nextInt();
+		int i=1;
+		int sum=0;
+		while(i<=x)
+		sum=sum+x;
+		System.out.println(sum);
+	}
+}
