7c47995da411cd432e6f72623aebd58ec3205291
govarshini <govarshiniarumugam@gmail.com>
2017-07-04 15:28:50 +0530

    Create sun.java

---
+++ sun.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class sun
+{
+	public static void main(String args[])
+	{
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		int i;
+		int sum=0;
+		//while(i<=a)
+		for(i=0;i<=a;i++)
+		sum=sum+i;
+		System.out.print(sum);
+	}
+}
